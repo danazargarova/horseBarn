66856750015da465d3073c11ce5a73a2550d5bb1
danazargarova <dzargarova6105@bths.edu>
2018-11-15 14:47:26 -0500

    2e234r

---
+++ src/src/HorseRunner.java
@@ -0,0 +1,10 @@
+public class HorseRunner {
+    public static void main(String[] args)
+    {
+        Horse horse1=new Mustang("Mothman", 1200);
+        Horse horse2=new Mustang("Sasquatch", 1100);
+        Horse horse3=new Mustang("Mr Levin", 1140);
+
+        Horse[] horses = new
+    }
+}
